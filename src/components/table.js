6699841c44e0ba2d0125b602071597e27faf90de
Goldpm <lowedavidadam@gmail.com>
2019-12-20 02:04:26 -0500

    turned data into for loop

--- src/components/table.js
+++ src/components/table.js
@@ -3,19 +3,7 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 import empArr from "./empArr";
 
-const empArr = [            
-    "Karen Drakkie",
-    "John Hertz",
-    "Cody Parker",
-    "Conrad Frazier",
-    "Sophia Wright",
-    "Debbie Stewart",
-    "Austin Bridges",
-    "Jenny Robinson",
-    "Donald Henderson",
-    "Tracy Lowe",
-    "Crystal Swarz",
-    "Keli Richards"]
+const numberOfEmploy = 32
 
 const branches = [
     "Sales",
@@ -41,69 +29,16 @@ const columns = [{
   sort: true
 }];
 
-const products = [
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[0],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[1],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[2],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[3],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[4],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[5],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[6],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[7],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[8],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[9],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[10],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: empArr[11],
-  branch: branches[Math.floor(Math.random()*6)]
-},
-];
+
+const products = [];
  
+for (let i = 0; i < numberOfEmploy; i++) {
+    products.push({
+        id: Math.floor(Math.random()*899999)+100000,
+        name: empArr[i],
+        branch: branches[Math.floor(Math.random()*6)] 
+    })
+}
 
  
 export default () =>
