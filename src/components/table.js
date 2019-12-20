271c3813e77f9617f7b09b76bfea7cde855c58ab
Goldpm <lowedavidadam@gmail.com>
2019-12-20 01:47:34 -0500

    added table data

--- src/components/table.js
+++ src/components/table.js
@@ -24,69 +24,7 @@ const branches = [
     "Operations",
     "Human Resources"
 ]
-const products = [
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-{
-  id: Math.floor(Math.random()*899999)+100000,
-  name: "Milk",
-  branch: 20
-},
-];
- 
+
 const columns = [{
   dataField: 'id',
   text: 'Employee ID',
@@ -98,8 +36,74 @@ const columns = [{
   filter: textFilter()
 }, {
   dataField: 'branch',
-  text: 'Employee Branch'
+  text: 'Employee Branch',
+  sort: true
 }];
+
+const products = [
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[0],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[1],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[2],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[3],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[4],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[5],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[6],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[7],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[8],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[9],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[10],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+{
+  id: Math.floor(Math.random()*899999)+100000,
+  name: empArr[11],
+  branch: branches[Math.floor(Math.random()*6)]
+},
+];
+ 
+
  
 export default () =>
   <BootstrapTable keyField='id' data={ products } columns={ columns } filter={ filterFactory() } />
