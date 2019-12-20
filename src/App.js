c2f7d65216cc64e92eccef0ede9f252e0c8d4d3d
Goldpm <lowedavidadam@gmail.com>
2019-12-20 01:30:18 -0500

    temporary table test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center">User Directory</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th className="active">Name</th>
+            <th>Branch</th>
+            <th>Id</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Karen Drakkie</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>John Hertz</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Cody Parker</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Conrad Frazier</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Sophia Wright</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Debbie Stewart</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Austin Bridges</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Jenny Robinson</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Donald Henderson</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Tracy Lowe</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Crystal Swarz</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+          <tr>
+            <td>Keli Richards</td>
+            <td></td>
+            <td> {Math.floor(Math.random()*899999)+100000} </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
