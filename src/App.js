38456efd20bb601ee33171050568936316a7480b
Goldpm <lowedavidadam@gmail.com>
2019-12-20 01:47:45 -0500

    added instruction h4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-center">User Directory</h1>
+      <h4 className="text-center">Click on a category to sort</h4>
     <BootstrapTable/>
     </div>
   );
